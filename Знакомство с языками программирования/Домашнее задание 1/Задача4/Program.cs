4915756b4edad279a2b4fe82334b24e3ca08ab68
Romanov Aleksej <roman-off13@mail.ru>
2022-10-05 21:32:14 +0300

    Задача4

---
+++ Знакомство с языками программирования/Домашнее задание 1/Задача4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int Count = 1;
+
+if (n < Count)
+
+{
+    Console.WriteLine("Введите положительное число больше нуля");
+}
+
+while (Count <= n)
+{
+    if (Count%2 == 0)    {Console.WriteLine(Count + "");}
+    Count ++;
+}
