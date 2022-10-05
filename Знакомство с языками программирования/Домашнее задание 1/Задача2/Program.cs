fa20c5256860aa22514cb971153bde7d2c5a3c18
Romanov Aleksej <roman-off13@mail.ru>
2022-10-05 21:03:22 +0300

    Задача2

---
+++ Знакомство с языками программирования/Домашнее задание 1/Задача2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (a > b|| a > x)
+{
+    Console.WriteLine($"Число {a} - max.");
+}
+if (b > a|| b > x)
+{
+    Console.WriteLine($"Число {b} - max.");
+}
+if (x > b|| x > a )
+{
+    Console.WriteLine($$"Число {x} - max.");
+}
