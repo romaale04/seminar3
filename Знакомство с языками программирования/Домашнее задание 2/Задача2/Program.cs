fa4cb3c155ece7069b58cea7960f9965d155ec84
Romanov Aleksej <roman-off13@mail.ru>
2022-10-09 21:18:31 +0300

    Задача2

---
+++ Знакомство с языками программирования/Домашнее задание 2/Задача2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 
+// Не использовать строки для расчета.
+
+Console.WriteLine("Программа удаляет вторую цифру любого трехзначного числа");
+int randomNumber = new Random().Next(100,1000);
+Console.WriteLine("Число: " + randomNumber);
+
+int number = (randomNumber / 100) * 10 + randomNumber % 10;
+
+    Console.WriteLine($"Число без второй цифры: {number}");
