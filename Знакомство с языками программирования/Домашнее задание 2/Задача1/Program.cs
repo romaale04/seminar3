1df57fd057643ce6dabd039fb7eb091b931a2ad3
Romanov Aleksej <roman-off13@mail.ru>
2022-10-09 20:51:34 +0300

    Задача1

---
+++ Знакомство с языками программирования/Домашнее задание 2/Задача1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+// Не использовать строки для расчета.
+
+Console.WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 & number < 1000)
+{
+    int result = number % 100;
+    int secondNumber = result/10;
+    System.Console.WriteLine(secondNumber);
+}
+else
+{
+    System.Console.WriteLine("Число не трехзначное");
+}
