82ec3bc0695f1c4b3fff6cf6962ba915a7fa5c67
Romanov Aleksej <roman-off13@mail.ru>
2022-10-17 18:21:12 +0300

    Задача1

---
+++ Знакомство с языками программирования/Домашнее задание 4/Задача1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B (и в нулевую степень).
+// Использовать свои функции, не использовать Math.Pow
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+
+
+System.Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите натуральное число b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b >= 0)
+{
+    int result = 1;
+    for (int i = 0; i < b; i++ )
+    {
+        result = result * a;
+    }
+
+   Console.WriteLine($"Число {a} в степени {b} равно {result}"); 
+}
+else
+{
+    Console.WriteLine($"Число {b} - ненатуральное"); 
+}
+
